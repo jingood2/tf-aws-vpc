379d9db39e80746b3120cbca9f093b287e6bbe1f
james <james@pui-Macmini.T-mobile.com>
2023-02-21 04:15:54 +0900

    update

--- provider.tf
+++ provider.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 4.51.0"
     }
   }
 
@@ -16,13 +16,13 @@ terraform {
   #  workspaces {
   #    prefix = "helloworld-"
   #  }
-  #}
+  #} 
 }
 
 # Provider Block
 provider "aws" {
   region  = var.aws_region
-  profile = "default"
+  profile = "jingood2"
 }
 /*
 Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal
