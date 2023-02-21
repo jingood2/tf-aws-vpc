447c6a9713f7318f0f6146414269e9b2688f4487
james <james@pui-Macmini.T-mobile.com>
2023-02-21 15:33:18 +0900

    update aws provider profile

--- provider.tf
+++ provider.tf
@@ -22,7 +22,7 @@ terraform {
 # Provider Block
 provider "aws" {
   region  = var.aws_region
-  profile = "jingood2"
+
 }
 /*
 Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal
