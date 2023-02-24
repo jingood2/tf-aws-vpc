067bdeb5d044f046dd1f23be79795b8b1fa4d129
james <james@pui-Macmini.local>
2023-02-25 06:39:26 +0900

    remove endpoints

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ module "vpc" {
 ################################################################################
 module "vpc_endpoints" {
   source = "./modules/vpc-endpoints"
-  create = true
+  create = false
 
   vpc_id             = module.vpc.vpc_id
   security_group_ids = [data.aws_security_group.default.id]
